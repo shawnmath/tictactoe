2877d5d183b136d812924dac468c4b5644724cc4
Shawn Mathew <shawn.mathew@gmail.com>
2017-10-25 20:52:07 -0400

    start on logic for checking win

--- tictactoe.js
+++ tictactoe.js
@@ -30,11 +30,11 @@
 	-------------------*/
 	function addSymbol(square) {
 		var x = document.createElement('span');
-		x.classList.add('symbol', 'Xsymbol');
+		x.classList.add('Xsymbol');
 		x.innerHTML = 'X';
 
 		var o = document.createElement('span');
-		o.classList.add('symbol', 'Osymbol');
+		o.classList.add('Osymbol');
 		o.innerHTML = 'O';
 		
 		( gameState.player === 1 ) ? square.appendChild(x) : square.appendChild(o);				
@@ -47,11 +47,15 @@
 	function updateGame() {
 		var squares = [...document.querySelectorAll('.square')];	
 		squares.map( square => {		
-			square.addEventListener('click', function(e) {
-				if( this.querySelector('.symbol') === null ) {
-					addSymbol(square);
-					gameState.playerMoves++;				
+			square.addEventListener('click', function(e) {				
+				if( !this.hasChildNodes() ) {
+					addSymbol(square);									
 					update_player();
+
+					gameState.playerMoves++;
+					if( gameState.playerMoves > 4 ) {
+						checkWin(this);
+					}
 				}
 				else {
 					e.preventDefault();				
@@ -59,6 +63,36 @@
 			});	
 		});
 	}
+
+
+	function checkWin(square) {
+		// data-coord of selected square
+		let sel_coord = square.dataset.coord;
+		// Symbol of selected square (X, O)
+		let sel_symbol = square.querySelector('span').classList;
+		// Class of the parent row selected square is in
+		let row = square.parentNode.classList;
+
+		// Add class to selected square to query adjacent squares excluding selected square
+		// Remove class from previously selected square
+		if( document.querySelector('.sel_square') !== null ) {
+			document.querySelector('.sel_square').classList.remove('sel_square');
+		}
+		square.classList.add('sel_square');
+
+		// Get values in same column as selected square
+		let col_vals = document.querySelectorAll(`[data-coord="${sel_coord}"]:not(.sel_square)`);
+		// Get values in same row as selected square
+		let row_vals = document.querySelectorAll(`.${row} .square:not([data-coord="${sel_coord}"])`);		
+
+		console.log(row);
+		
+		console.log(col_vals);	
+		console.log(row_vals);
+
+		
+	}
+
 
 	// Init Game
 	return {
